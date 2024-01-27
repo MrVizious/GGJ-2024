b5c6b82a6ef702d8aec60d12117fa7b478724ec5
MrVizious <mrvizious@gmail.com>
2024-01-27 21:38:06 +0100

    Francisca Animations

--- Assets/_Home_/Scripts/Actor.cs
+++ Assets/_Home_/Scripts/Actor.cs
@@ -1,7 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Actor : ShowAgent
 {
+    private Vector3 lastPosition;
+    private void Start()
+    {
+        lastPosition = transform.position;
+    }
+    private void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        animator.SetFloat("Speed", (lastPosition - currentPosition).magnitude * 100f);
+        lastPosition = currentPosition;
+    }
 }
