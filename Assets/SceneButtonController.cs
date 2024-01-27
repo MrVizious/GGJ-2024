4ac67109fad73186daf391263c51fe43cbaf7e4f
SerghG <38987734+SerghG@users.noreply.github.com>
2024-01-27 23:28:34 +0100

    ShowSceneButtonTerminada

--- Assets/SceneButtonController.cs
+++ Assets/SceneButtonController.cs
@@ -8,16 +8,19 @@ public class SceneButtonController : MonoBehaviour
     [SerializeField] List<GameObject> camButtonsList = new List<GameObject>(); 
 
     [SerializeField]  GameObject desactivatedButton = null;
+
+    [SerializeField]  GameObject bazingaButton;
+    [SerializeField] int numKeyDeactivated = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        numKeyDeactivated = 0;
+        bazingaButton.GetComponent<ExtendedButton>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void desactivateNewButton(GameObject newButton){
@@ -33,5 +36,18 @@ public class SceneButtonController : MonoBehaviour
 
     public void deactivateSoundButton(GameObject soundButton){
         soundButton.GetComponent<ExtendedButton>().enabled = false;
+    }
+
+    public void deactivateKeyButton(GameObject keyButton){
+        keyButton.GetComponent<ExtendedButton>().enabled = false;
+        numKeyDeactivated++;
+        if(numKeyDeactivated == 2){
+            bazingaButton.GetComponent<ExtendedButton>().enabled = true;
+            Debug.Log("Turbobazinga activado");
+        }
+    }
+
+    public void deactivateBazingaButton(){
+          bazingaButton.GetComponent<ExtendedButton>().enabled = false;
     }
 }
