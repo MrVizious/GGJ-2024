da2a557d7374c943c47b10203f1759b9dae8a852
MrVizious <mrvizious@gmail.com>
2024-01-27 12:31:18 +0100

    Sound Manager skeleton created, basic functioning

--- Assets/_Home_/Scripts/SoundManager.cs
+++ Assets/_Home_/Scripts/SoundManager.cs
@@ -2,18 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Audio;
+using ExtensionMethods;
+using Sirenix.OdinInspector;
 
 public class SoundManager : MonoBehaviour
 {
     public UnityEvent<AudioClip> onSoundPlay = new UnityEvent<AudioClip>();
     private HashSet<AudioClip> playingSounds = new HashSet<AudioClip>();
+    [Button]
     public void PlaySound(AudioClip audioClip)
     {
         if (playingSounds.Contains(audioClip))
         {
             Debug.Log("Audio is already playing");
+            return;
         }
-        // TODO: Play sound
+        playingSounds.Add(audioClip);
+        AudioSourceExtended source = AudioManager.Instance.PlaySound(audioClip);
+        source.onRelease += () => OnSoundStop(audioClip);
+    }
 
+    private void OnSoundStop(AudioClip audioClip)
+    {
+        Debug.Log("Sound " + audioClip + " ended!");
+        playingSounds.Remove(audioClip);
     }
 }
